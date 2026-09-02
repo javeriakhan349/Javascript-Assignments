a59f5170be9f2209dea9652b0a26b69ad5f089ad
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-02 13:16:21 -0300

    Chap 3

---
+++ Assignment 3/app.js
@@ -0,0 +1,26 @@
+//  CHAP #03 :VARIABLES FOR NUMBERS 
+
+var age = "17 years old"
+alert("Iam " + age)
+
+
+// 2. Declare & initialize a variable to keep track of how manytimes a visitor has visited a web page. Show his/hernumber of visits on your web page. For example: “You have visited this site N times”
+var visit = 0;
+visit++;
+alert("You have visit this website "+visit+ " times ")
+
+
+// 3. Declare a variable called birthYear & assign to it yourbirth year. Show the following message in your browser:
+var birthYear = 2010
+document.write("My birth year is  " + birthYear)
+
+
+// 4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variablesthe following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+var visitorName = "John Doe"
+var productTitle = "T-shirt(s)"
+var quantity = "5"
+document.write(visitorName + "  ordered  " +quantity + productTitle + " on XYZ Clothing store")

60ef9d3ab583d4757ae87173e0a002aae30da8f3
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-02 13:52:44 -0300

    chap 5

---
+++ Assignment 5/app.js
@@ -0,0 +1,8 @@
+// CHAPTER # 05 : MATH EXPRESSIONS
+
+// 1. Write a program that take two numbers & add them in a new variable.Show the result in your browser
+var num1 = +prompt("Enter a number!")
+var num2 = +prompt("Enter a number!")
+
+sum = num1+num2;
+document.write("Sum of  " +num1+ " and  "+ num2 + "  is  " + sum)

316f754ffd2c4830881111df507c61070069358a
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-02 18:39:27 -0300

    Chap 5

--- Assignment 5/app.js
+++ Assignment 5/app.js
@@ -4,5 +4,31 @@
 var num1 = +prompt("Enter a number!")
 var num2 = +prompt("Enter a number!")
 
-sum = num1+num2;
-document.write("Sum of  " +num1+ " and  "+ num2 + "  is  " + sum)
+ var sum = num1+num2;
+document.write("Sum of  " + num1+ " and  "+ num2 + "  is  " + sum)
+
+
+// 2. Repeat task1 for subtraction, multiplication, division & modulus.
+//Subtraction
+
+ var difference = num1-num2;
+document.write("<br> Difference of...  " + num1+ " and  "+ num2 + "  is  " + difference)
+
+
+//Multiplication
+
+ var product = num1*num2;
+document.write("<br>Product of...  " + num1+ " and  "+ num2 + "  is  " + product)
+
+
+//Division
+
+ var quotient = num1+num2;
+document.write(" <br> Division of ....  " + num1+ " and  "+ num2 + "  is  " + quotient)
+
+
+
+//Modulus
+
+ var remainder = num1%num2;
+document.write(" <br> Modulus of ....  " + num1+ " and  "+ num2 + "  is  " + remainder)

ea9ae66285958abc4ab0dedeca0f3c6437dc1d83
javeriakhan349 <javeriakhanjk349@gmail.com>
2026-09-22 00:59:58 +0500

    Chap 20

--- Assignment 11/app.js
+++ Assignment 11/app.js
@@ -42,3 +42,75 @@ fruits = [ "apple" , "banana" , "mango" , "orange" , "strawberry"]
 for( i = 0 ; i<=4 ; i++){
 document.write(`Element at index ${i} is ${fruits[i]} <br>`)
 }
+
+
+// 6. Generate the following series in your browser. See
+// example output.
+var counting = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+document.write(`<br> <h1>Counting </h1>`)
+document.write(` ${counting}`)
+
+var reverse = [ 10,9,8,7,6,5,4,3,2,1]
+
+document.write(`<br> <h1>Reverse Counting </h1>`)
+document.write(` ${reverse}`)
+
+var even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+document.write(`<br> <h1>Even Numbers </h1>`)
+document.write(` ${even}`)
+
+var odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+document.write(`<br> <h1>Odd Numbers </h1>`)
+document.write(` ${odd}`)
+
+// 7. You have an array Write a program to enable “search by user input” in an array.After searching, prompt the user whether the given item is
+// found in the list or not.
+var items = [ "apple pie" , "cookie" , "cake" , "chips" , "patties"]
+var userItem = prompt("Enter a sweet item...")
+var flag = "false";
+for( i=0 ; i<=items.length ; i++){
+    if(items[i] === userItem)
+    {
+        flag = "true"
+        document.write(`${userItem} is avaliable at index  ${i}`)
+    }
+}
+if(flag === "false"){
+
+document.write(`${userItem} is not avaliable `)
+
+}
+
+// 8. Write a program to identify the largest number in the
+// given array.
+var largest = [24, 53, 78, 91, 12]
+var number = largest[0]
+document.write(`<br> Array items:    ${largest} <br>`)
+for( k=0 ; k<=largest.length ; k++){
+    if(largest[k] > number){
+        number = largest[k]
+        
+    }
+}document.write(`The largest number is  ${number}`)
+
+
+// 9. Write a program to identify the smallest number in the
+// given array.
+A = [24, 53, 78, 91, 12]
+var smallest = A[0]
+document.write(`<br> <br><br>Array items:    ${A} <br>`)
+for ( t=0 ; t<=smallest.length ; t++){
+    if(A[t] < smallest){
+        smallest = A[t]
+    }
+}document.write(`The Smallest number is  ${smallest}`)
+
+
+// 10. Write a program to print multiples of 5 ranging 1 to 100.
+var multiples =[]
+for( u=0 ; u<=100 ; u+=5){
+    multiples.push(u)
+
+} document.write(`Multiples of 5: `)
+ document.write("<br>"+multiples)
